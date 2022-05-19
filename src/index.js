f0bc335cb436e79d95bb33fe18f37b5542ffedbd
Josh Mathiak <jmathiak4@gmail.com>
2022-05-18 20:53:44 -0500

    Added placeShip and receieveAttack

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-function createShip(length) {
+function createShip(name, direction, length, startingCord, endingCord) {
   var ship = {};
-
+  ship.name = name;
   ship.length = length;
+  ship.startingCord = startingCord;
+  ship.endingCord = endingCord;
   ship.health = [];
   ship.sunk = false;
 
-  for (let i = 0; i < length; i++) {
-    ship.health.push({
-      shipPoint: i,
-      hitStatus: "false",
-    });
+  if (direction == "horizontal") {
+    for (let i = 0; i < length; i++) {
+      ship.health.push({
+        shipPoint: i,
+        hitStatus: "false",
+        coordinates: {
+          xCord: startingCord.xCord + i,
+          yCord: startingCord.yCord,
+        },
+      });
+    }
+  } else if (direction == "vertical") {
+    for (let i = 0; i < length; i++) {
+      ship.health.push({
+        shipPoint: i,
+        hitStatus: "false",
+        coordinates: {
+          xCord: startingCord.xCord,
+          yCord: startingCord.yCord + i,
+        },
+      });
+    }
   }
 
   ship.isSunk = function () {
@@ -32,7 +51,7 @@ function createShip(length) {
 
 function createGameBoard() {
   var gameBoard = {};
-
+  gameBoard.ships = [];
   gameBoard.board = [
     ["empty", "empty", "empty", "empty", "empty", "empty", "empty"],
     ["empty", "empty", "empty", "empty", "empty", "empty", "empty"],
@@ -43,16 +62,55 @@ function createGameBoard() {
     ["empty", "empty", "empty", "empty", "empty", "empty", "empty"],
   ];
 
-  gameBoard.placeShip = function (direction, length, yCord, xCord) {
-    let ship = createShip(length);
-    if (direction === "horizontal") {
+  gameBoard.placeShip = function (direction, name, length, yCord, xCord) {
+    let endingCord = {};
+    if (direction == "horizontal") {
+      endingCord.endingXCord = xCord + length;
+      endingCord.endingYCord = yCord;
+    } else if (direction == "vertical") {
+      endingCord.endingXCord = xCord;
+      endingCord.endingYCord = yCord + length;
+    }
+    let ship = createShip(
+      name,
+      direction,
+      length,
+      { startingXCord: xCord, startingYCord: yCord },
+      endingCord
+    );
+    gameBoard.ships.push(ship);
+    if (direction == "horizontal") {
       for (let i = 0; i < length; i++) {
-        this.board[yCord][xCord + i] = ship.health[i];
+        let placeObj = {
+          name: ship.name,
+          spot: ship.health[i],
+        };
+        this.board[yCord][xCord + i] = placeObj;
       }
-    } else if (direction === "vertical") {
+    } else if (direction == "vertical") {
       for (let i = 0; i < length; i++) {
+        let placeObj = {
+          name: ship.name,
+          spot: ship.health[i],
+        };
         this.board[yCord + i][xCord] = ship.health[i];
       }
+    }
+  };
+
+  gameBoard.receiveAttack = function (yCord, xCord) {
+    if (
+      this.board[yCord][xCord] !== "empty" &&
+      this.board[yCord][xCord] !== "miss"
+    ) {
+      let shipName = this.board[yCord][xCord];
+      let filteredArr = this.ships.filter((sh) => sh.name !== shipName);
+      let ship = filteredArr[0];
+      let pointArr = ship.health.filter(
+        (point) =>
+          point.coordinates.xCord != xCord && point.coordinates.yCord != yCord
+      );
+      ship.hit(pointArr[0].shipPoint);
     }
   };
 
@@ -60,8 +118,10 @@ function createGameBoard() {
 }
 
 let b1 = createGameBoard();
-b1.placeShip("horizontal", 4, 0, 3);
+b1.placeShip("horizontal", "Battleship", 4, 0, 3);
+b1.receiveAttack(0, 3);
 console.log(b1.board);
+console.log(b1.board[0][3]);
 exports.createShip = createShip;
 exports.createGameBoard = createGameBoard;
 
@@ -80,4 +140,14 @@ exports.createGameBoard = createGameBoard;
     Each new sub-array is a y incrementation
     Each index inside of the subarray is a x- incrementation
     Board will be y = 0 from the top x = 0 from the left
+*/
+
+/*
+    Ships:
+    Carrier- 5
+    Battleship - 4
+    Cruiser - 3
+    Submarine - 3
+    Destroyer - 2
+
 */
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,18 +1,18 @@
 const gameModules = require("./index.js");
 
 test("ship returns not sunk", () => {
-  let ship1 = gameModules.createShip(4);
+  let ship1 = gameModules.createShip("Battleship", 4);
   expect(ship1.isSunk()).toBe(false);
 });
 
 test("ship is hit in spot 1", () => {
-  let ship1 = gameModules.createShip(4);
+  let ship1 = gameModules.createShip("Battleship", 4);
   ship1.hit(1);
   expect(ship1.health[1].hitStatus).toBe("true");
 });
 
 test("ship is sunk", () => {
-  let ship1 = gameModules.createShip(5);
+  let ship1 = gameModules.createShip("Carrier", 5);
   ship1.hit(0);
   ship1.hit(1);
   ship1.hit(2);
@@ -23,40 +23,61 @@ test("ship is sunk", () => {
 
 test("Ship is placed horizontally", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("horizontal", 4, 0, 3);
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
   expect(b1.board[0][3]).toStrictEqual({ shipPoint: 0, hitStatus: "false" });
 });
 
 test("Ship is placed horizontally", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("horizontal", 4, 0, 3);
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
   expect(b1.board[0][4]).toStrictEqual({ shipPoint: 1, hitStatus: "false" });
 });
 test("Ship is placed horizontally", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("horizontal", 4, 0, 3);
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
   expect(b1.board[0][5]).toStrictEqual({ shipPoint: 2, hitStatus: "false" });
 });
 test("Ship is placed horizontally", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("horizontal", 4, 0, 3);
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
   expect(b1.board[0][6]).toStrictEqual({ shipPoint: 3, hitStatus: "false" });
 });
 
 test("Ship is placed vertically", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("vertical", 3, 3, 2);
+  b1.placeShip("vertical", "Cruiser", 3, 3, 2);
   expect(b1.board[3][2]).toStrictEqual({ shipPoint: 0, hitStatus: "false" });
 });
 
 test("Ship is placed vertically", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("vertical", 3, 3, 2);
+  b1.placeShip("vertical", "Cruiser", 3, 3, 2);
   expect(b1.board[4][2]).toStrictEqual({ shipPoint: 1, hitStatus: "false" });
 });
 
 test("Ship is placed vertically", () => {
   let b1 = gameModules.createGameBoard();
-  b1.placeShip("vertical", 3, 3, 2);
+  b1.placeShip("vertical", "Cruiser", 3, 3, 2);
   expect(b1.board[5][2]).toStrictEqual({ shipPoint: 2, hitStatus: "false" });
+});
+
+test("Ship is placed and sunk after 3 hits", () => {
+  let b1 = gameModules.createGameBoard();
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
+  b1.receiveAttack(0, 3);
+  expect(b1.board[0][3].spot.hitStatus).toBe("true");
+});
+
+test("Coordinates for board[0][3] should match. x = 3", () => {
+  let b1 = gameModules.createGameBoard();
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
+  b1.receiveAttack(0, 3);
+  expect(b1.board[0][3].spot.coordinates.xCord).toBe(3);
+});
+
+test("Coordinates for board[0][3] should match. y = 0", () => {
+  let b1 = gameModules.createGameBoard();
+  b1.placeShip("horizontal", "Battleship", 4, 0, 3);
+  b1.receiveAttack(0, 3);
+  expect(b1.board[0][3].spot.coordinates.yCord).toBe(0);
 });
